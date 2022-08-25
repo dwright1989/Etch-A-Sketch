1feca733b1cc6c9e0fe009129bbcae8ef05df4c4
Debbie Wright <debbiewright@live.co.uk>
2022-08-25 12:57:39 +0100

    Changed to use flex wrap.  All cells now same size (height and width)

--- script.js
+++ script.js
@@ -7,25 +7,16 @@ Create the Etch-A-Sketch board using the dimension value chosen by user
 or the fault of 16x16
 */
 function createBoard(size){
-    for(let i=0; i<size; i++){
+    // create each div e.g. if 16x16 = 256
+    let numOfDivs = dimension*dimension;
+    for(let i=0; i<numOfDivs; i++){
         // create a new row div in the DOM within the grid container
         let gridContainer = document.getElementById("gridContainer");
-        let rowDiv = document.createElement("div");
-        rowDiv.classList.add("rowDiv");
-        rowDiv.classList.add("border");
-        
-        for(let j=0; j<size; j++){
-            let columnDiv = document.createElement("div");
-            columnDiv.classList.add("columnDiv");
-            columnDiv.classList.add("border");
-            let columnWidth = 100/size+"%";
-            columnDiv.style.width = columnWidth;
-            columnDiv.style.height = columnWidth;
-            columnDiv.innerHTML="Column " + (j+1);
-            rowDiv.appendChild(columnDiv);
-        }
-
-        gridContainer.appendChild(rowDiv);
+        let cellDiv = document.createElement("div");
+        cellDiv.classList.add("border");
+        cellDiv.classList.add("cell");
+        cellDiv.innerHTML = "Cell " + (i+1);
+        gridContainer.appendChild(cellDiv);
         
     }
 }
