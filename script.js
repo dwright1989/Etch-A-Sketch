bf7e716c2ba206548f240ffbd304ba6a5ea6a0d9
Debbie Wright <debbiewright@live.co.uk>
2022-08-25 13:40:40 +0100

    Dynamic change of flex basis

--- script.js
+++ script.js
@@ -8,14 +8,31 @@ or the fault of 16x16
 */
 function createBoard(size){
     // create each div e.g. if 16x16 = 256
-    let numOfDivs = dimension*dimension;
+    let numOfDivs = size*size;
+
+    // set the correct flex-basis based on the dimension
+    let basis = 100/(size+1)+"%";
+    console.log("flexBasis " + basis);
+    /*const cells = document.querySelectorAll('.cell');
+    console.log("the cells: " + cells);
+
+
+    cells.forEach(cell => {
+
+        console.log("in the for each.  Cell: " + cell);
+        cell.style.flexBasis = basis;
+    });*/
+
     for(let i=0; i<numOfDivs; i++){
         // create a new row div in the DOM within the grid container
         let gridContainer = document.getElementById("gridContainer");
         let cellDiv = document.createElement("div");
         cellDiv.classList.add("border");
         cellDiv.classList.add("cell");
-        cellDiv.innerHTML = "Cell " + (i+1);
+        console.log("the cell div basis is: " + cellDiv.style.flexBasis)
+        cellDiv.style.flexBasis = basis;
+        //cellDiv.innerHTML = "Cell " + (i+1);
+        // add an ID so each cell is identifiable
         gridContainer.appendChild(cellDiv);
         
     }
